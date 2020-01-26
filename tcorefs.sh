e66c539b16cbcd96b6d66eb01955fb41cb2a06dc
tryll.com <github@tryll.com>
2020-01-27 00:22:42 +0100

    Networking

--- tcorefs.sh
+++ tcorefs.sh
@@ -10,7 +10,7 @@ echo Host: Ensuring qemu and binfmt are installed
 sudo apt-get install -y qemu-user-static binfmt-support busybox-static 
 sudo update-binfmts  --enable qemu-arm
 
-ROOT=target/rootfs
+ROOT=rootfs
 TARGET=$ROOT/bootstrap
 mkdir -p $TARGET
 
@@ -35,35 +35,35 @@ echo Target: Downloading debian base packages
 
 
 # Bootstrap base filesytem
-dpkg -x $TARGET/base-files* target/rootfs
-dpkg -e $TARGET/base-files* target/rootfs/tmp/
+dpkg -x $TARGET/base-files* $ROOT
+dpkg -e $TARGET/base-files* $ROOT/tmp/
 
 # Initate base busybox
-dpkg -x $TARGET/busybox_* target/rootfs
-dpkg -x $TARGET/libc6* target/rootfs
-sudo /usr/sbin/chroot target/rootfs /bin/busybox --install
+dpkg -x $TARGET/busybox_* $ROOT
+dpkg -x $TARGET/libc6* $ROOT
+sudo /usr/sbin/chroot $ROOT /bin/busybox --install
 
 
-dpkg -x $TARGET/dpkg* target/rootfs
-dpkg -x $TARGET/libselinux1* target/rootfs
-dpkg -x $TARGET/libpcre3* target/rootfs
-dpkg -x $TARGET/zlib1g* target/rootfs
-dpkg -x $TARGET/libc-bin* target/rootfs
-dpkg -x $TARGET/liblzma5* target/rootfs
-dpkg -x $TARGET/libbz2-1.0* target/rootfs
-dpkg -x $TARGET/tar* target/rootfs
-dpkg -x $TARGET/libacl1* target/rootfs
-dpkg -x $TARGET/libattr1* target/rootfs
-dpkg -x $TARGET/coreutils* target/rootfs
+dpkg -x $TARGET/dpkg* $ROOT
+dpkg -x $TARGET/libselinux1* $ROOT
+dpkg -x $TARGET/libpcre3* $ROOT
+dpkg -x $TARGET/zlib1g* $ROOT
+dpkg -x $TARGET/libc-bin* $ROOT
+dpkg -x $TARGET/liblzma5* $ROOT
+dpkg -x $TARGET/libbz2-1.0* $ROOT
+dpkg -x $TARGET/tar* $ROOT
+dpkg -x $TARGET/libacl1* $ROOT
+dpkg -x $TARGET/libattr1* $ROOT
+dpkg -x $TARGET/coreutils* $ROOT
 
-printf '#!/bin/sh\nless $*\n' > $TARGET/../usr/bin/pager
-chmod 755 $TARGET/../usr/bin/pager
+printf '#!/bin/sh\nless $*\n' > $ROOT/usr/bin/pager
+chmod 755 $ROOT/usr/bin/pager
 
 
 
 # init base filesystem
-sudo /usr/sbin/chroot target/rootfs /bin/sh /tmp/postinst
-echo "/usr/bin/dpkg -i --force-all bootstrap/*.deb" | sudo chroot target/rootfs sh -C
+sudo /usr/sbin/chroot $ROOT /bin/sh /tmp/postinst
+echo "/usr/bin/dpkg -i --force-all bootstrap/*.deb" | sudo chroot $ROOT sh -C
 
 # cleanup
 sudo rm -rf $TARGET
@@ -74,8 +74,10 @@ sudo rm -rf $ROOT/usr/share/man/*
 P=$ROOT/usr/lib/*/gconv
 sudo rm $P/libCNS* $P/BIG5* $P/IBM* $P/GB* $P/libJISX0213*
 
-du -h $ROOT
-
+# networking
+NET=$ROOT/etc/network
+mkdir -p $NET/if-down.d  $NET/if-post-down.d  $NET/if-pre-up.d  $NET/if-up.d  $NET/interfaces.d
+printf 'source /etc/network/interfaces.d/*\nauto lo\niface lo inet loopback\nallow-hotplug eth0\niface eth0 inet dhcp\n' > $NET/interfaces
 
 
 #cp setup.sh target/rootfs/
